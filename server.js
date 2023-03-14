3a1f36a82c10bb1933d05df001dd5772b0ae77db
thom268f <tthors22@student.aau.dk>
2023-03-14 10:56:30 +0100

    help

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 let active_users = 0;
+let unique_users = 0;
+const operators = ['+', '-', '*', '/'];
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/front_page.html');
@@ -9,8 +11,14 @@ app.get("/", (req, res) => {
 
 app.get('/connect', (req, res) => {
     active_users++;
-    res.send(`{"newID": 1}`);
+    console.log(createID());
+    res.send(`{"newID": ${createID()}}`);
 })
+
+function createID(){
+    unique_users++;
+    return 1;
+}
 
 app.get('/disconnect', (req, res) => {
     active_users--;
@@ -18,8 +26,25 @@ app.get('/disconnect', (req, res) => {
 })
 
 app.get('/active-users', (req, res) => {
-    res.send(`{"userCount": ${active_users}}`);
+    let information = {userCount: active_users}
+    res.send(information);
 })
+
+app.get('/get-task', (req, res) => {
+    res.send(`{"userCount": ${active_users},
+                "task": ${createTask()}}`);
+})
+
+
+function createTask(){
+    const numbers = {
+        num1: Math.floor(Math.random() * 10) + 1,
+        num2: Math.floor(Math.random() * 10) + 1,
+        operator: operators[Math.floor(Math.random() * operators.length)]
+      };
+    return numbers;
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
