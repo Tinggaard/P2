5199e7f7549d7388b4435f1910da216de054cd10
T1ng <mail@t1ng.dk>
2023-03-14 11:15:20 +0100

    simple app.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Time: ', Date.now());
-  next();
-});
-
-app.use('/request-type', (req, res, next) => {
-  console.log('Request type: ', req.method);
-  next();
-});
-
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
