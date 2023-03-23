4d826d05032f5c0557c149f7696a0565f84caede
enevoldsen1oo <mhen22@student.aau.dk>
2023-03-23 10:05:17 +0100

    Import between client files, send task from server to client

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 // Define express and ws
-const express = require('express');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const { Server } = require('ws');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+import { WebSocketServer } from 'ws';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 // create object to send over WebSocket
 function Obj(type, data) {
@@ -10,32 +14,37 @@ function Obj(type, data) {
   this.data = data;
 }
 
+// const tasks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // express server implementation
-const server = express()
+const exServer = express()
   // make the entire /public directory available
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(dirname, 'public')))
   .listen(3000, () => console.log(`Listening on ${3000}`));
 
 // Create a new instance of ws server
-const wsServer = new Server({ server });
+const wsServer = new WebSocketServer({ server: exServer });
 // Print to console on client connected
-wsServer.on('connection', (ws) => {
+wsServer.on('connection', (webSocket) => {
+  // const available = new Obj('available', true);
   const id = new Obj('id', uuidv4());
-
   console.log(`${id.data}: *Connected*`);
   // Print number of clients connected
   console.log('Connected clients:', wsServer.clients.size);
 
   // at first connect, we send the ID to the client
-  ws.send(JSON.stringify(id));
+  webSocket.send(JSON.stringify(id));
+  // Send numbers for calc
+  const A = [7, 19];
+  const calcMessage = new Obj('calc', A);
+  webSocket.send(JSON.stringify(calcMessage));
 
   // log messages we get in
-  ws.on('message', (message) => {
+  webSocket.on('message', (message) => {
     console.log(`${id.data}: ${message}`);
   });
 
   // Print to console on client disconnected
-  ws.on('close', () => {
+  webSocket.on('close', () => {
     console.log(`${id.data}: *Disconnected*`);
     // Print number of clients connected
     console.log('Connected clients:', wsServer.clients.size);
@@ -49,3 +58,15 @@ setInterval(() => {
     client.send(JSON.stringify(time));
   });
 }, 1000);
+
+// Submitting a task to each client
+// setInterval(() => {
+//  let taskIndexX = 0;
+//  let taskIndexY = 1;
+//  wsServer.clients.forEach((client) => {
+//    const calcMessage = new Obj('calc', (tasks[taskIndexX], tasks[taskIndexY]));
+//    client.send(JSON.stringify(calcMessage));
+//    taskIndexX += 2;
+//    taskIndexY += 2;
+//   });
+// }, 1000);
