38b970666cc2d2a0f6816cee8bcb996778c59406
Tinggaard <mail@t1ng.dk>
2023-04-03 13:50:04 +0200

    Created class for main tasks
    Co-Authored-By: Aske <askeklitgaard@gmail.com>

--- src/server.js
+++ src/server.js
@@ -4,26 +4,24 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { v4 as uuidv4 } from 'uuid';
 import { WebSocketServer } from 'ws';
-import {
-  Obj, assignTask, nextPermutation,
-} from './control.js';
+import { Obj, Task } from './control.js';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-// Initializes all variables to be used by the server when dividing a task into subtasks
-const subtaskLength = 4;
-const currCombination = Array.from({ length: subtaskLength }, (_, i) => i + 1); // !const? (eslint)!
-const TSPnodes = 6;
-let currPerm = currCombination.slice();
-const c = new Array(currPerm.length).fill(0);
-let i = 0;
-let subTasks = [];
 const results = [];
 let resNr = 0;
 
-// eslint-disable-next-line max-len
-[subTasks, i, currPerm] = nextPermutation(currPerm, subtaskLength, c, i, currCombination, TSPnodes, 5);
+const weights = [
+  [1, 2, 3, 4, 5],
+  [4, 3, 2, 1, 3],
+  [7, 3, 6, 3, 6],
+  [8, 1, 100, 2, 7],
+  [1, 9, 8, 5, 13],
+];
+
+const example = new Task(5, weights);
+const iterator = example.getNextCombination();
 
 // express server implementation
 const exServer = express()
@@ -44,6 +42,13 @@ wsServer.on('connection', (webSocket) => {
   // at first connect, we send the ID to the client
   webSocket.send(JSON.stringify(id));
 
+  let problem = iterator.next();
+
+  if (!problem.done) {
+    const obj = new Obj('calc', problem.value);
+    webSocket.send(JSON.stringify(obj));
+  }
+
   // log messages we get in
   webSocket.on('message', (message) => {
     // determine type of data
@@ -53,18 +58,18 @@ wsServer.on('connection', (webSocket) => {
       case 'result':
         results[resNr] = `${data.data}`;
         resNr += 1;
-        console.log(data.data);
+        console.log(`recieved result: ${data.data}`);
+
+        problem = iterator.next(); // send new problem
+        if (!problem.done) {
+          const obj = new Obj('calc', problem.value);
+          webSocket.send(JSON.stringify(obj));
+        }
         break;
-        // do nothing
+      // do nothing
       default:
         break;
     }
-
-    if (subTasks.length === 0) {
-      // eslint-disable-next-line max-len
-      [subTasks, i, currPerm] = nextPermutation(currPerm, subtaskLength, c, i, currCombination, TSPnodes, 5);
-    }
-    assignTask(subTasks, webSocket);
   });
 
   // Print to console on client disconnected
