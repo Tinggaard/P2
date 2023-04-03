a97cfd1e6a0c2956fc9e3ebccab94e71001a4b40
Tinggaard <mail@t1ng.dk>
2023-04-03 14:15:19 +0200

    remove old disconnect implementation

--- src/public/client.js
+++ src/public/client.js
@@ -2,7 +2,6 @@ import { Obj, calcExp } from './provider.js';
 
 // first contact
 let websocket;
-let disconnecting = false;
 let rdyButton;
 
 function addWebSocketEventListeners() {
@@ -31,12 +30,8 @@ function addWebSocketEventListeners() {
         break;
       case 'calc':
         await calcExp(data, websocket);
-        if (disconnecting) {
-          websocket.close();
-          rdyButton.value = 'Connect';
-        }
         break;
-        // do nothing
+      // do nothing
       default:
         break;
     }
@@ -45,7 +40,6 @@ function addWebSocketEventListeners() {
 
 function rdySender() {
   if (rdyButton.value === 'Connect') {
-    disconnecting = false;
     websocket = new WebSocket(document.location.origin.replace(/^http/, 'ws'));
     console.log('connect');
     rdyButton.value = 'Disconnect';
@@ -63,9 +57,3 @@ rdyButton = document.querySelector('#connecterBtn');
 rdyButton.addEventListener('click', () => {
   rdySender();
 });
-
-document
-  .querySelector('#sendToServer')
-  .addEventListener('click', () => {
-    websocket.send('Hello server!');
-  });
