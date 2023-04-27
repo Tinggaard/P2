35fd042c039f945575f63c232af8875cb454f525
Maxnm47 <96955526+Maxnm47@users.noreply.github.com>
2023-04-27 12:25:26 +0200

    now working file upload

--- src/server.js
+++ src/server.js
@@ -8,54 +8,56 @@ import { Obj, Task } from './control.js';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-
+// initialization of variables
 let finishedSubtasks = 0;
-
-const weights = [
-  [0, 2, 3, 4, 5, 7, 7, 3, 0, 3, 6, 4, 5, 2],
-  [4, 0, 2, 1, 3, 8, 7, 3, 0, 3, 6, 4, 5, 2],
-  [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 4, 5, 2],
-  [8, 1, 100, 0, 7, 2, 7, 3, 0, 3, 6, 4, 5, 2],
-  [1, 9, 8, 5, 0, 7, 7, 3, 0, 3, 6, 4, 5, 2],
-  [7, 3, 0, 3, 6, 0, 7, 3, 0, 3, 6, 4, 5, 2],
-  [0, 2, 3, 4, 5, 7, 0, 3, 0, 3, 6, 4, 5, 2],
-  [4, 0, 2, 1, 3, 8, 7, 0, 0, 3, 6, 4, 5, 2],
-  [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 4, 5, 2],
-  [8, 1, 100, 0, 7, 2, 7, 3, 3, 0, 6, 4, 5, 2],
-  [1, 9, 8, 5, 0, 7, 7, 3, 0, 3, 0, 4, 5, 2],
-  [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 0, 5, 2],
-  [7, 3, 0, 3, 6, 0, 7, 3, 0, 3, 6, 4, 0, 2],
-  [0, 2, 3, 4, 5, 7, 0, 3, 0, 3, 6, 4, 5, 0],
-];
-
-const task = new Task(weights.length, weights);
-// A perhaps scuffed way to calculate total subtasks..
-const totalSubtasks = new Obj('totalSubtasks', ((task.nodeCount - 1) * (task.nodeCount - 2)));
-console.log(totalSubtasks);
-const iterator = task.getNextCombination();
-
+let iterator;
+let totalSubtasks;
+let task;
+let fileWeightsObj;
 // express server implementation
 const app = express();
 const appServer = app.use(express.static(path.join(dirname, 'public')))
   .listen(3000, () => console.log('Server running at http://localhost:3000'));
-
+let fileWeights;
 app.post('/server-weights', (req, res) => {
   let body = '';
   req.on('data', (chunk) => {
-    body += chunk.toString(); // convert Buffer to string
+    body += chunk.toString();
   });
   req.on('end', () => {
     try {
-      const data = JSON.parse(body);
-      console.log('Received data:', data);
-      // Do something with the data here
-      res.json({ status: 'success' });
+      // this is where it comes in
+      fileWeights = JSON.parse(body);
+      fileWeightsObj = new Obj('weights', fileWeights.weights);
+      task = new Task(fileWeightsObj.data.length, fileWeights.weights);
+      console.log(fileWeightsObj);
+      // A perhaps scuffed way to calculate total subtasks..
+      totalSubtasks = new Obj('totalSubtasks', ((task.nodeCount - 1) * (task.nodeCount - 2)));
+      iterator = task.getNextCombination();
     } catch (err) {
       console.error('Error parsing JSON:', err);
       res.status(400).json({ error: 'Invalid JSON data' });
     }
   });
 });
+
+// const weights = [
+//   [0, 2, 3, 4, 5, 7, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [4, 0, 2, 1, 3, 8, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [8, 1, 100, 0, 7, 2, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [1, 9, 8, 5, 0, 7, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [7, 3, 0, 3, 6, 0, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [0, 2, 3, 4, 5, 7, 0, 3, 0, 3, 6, 4, 5, 2],
+//   [4, 0, 2, 1, 3, 8, 7, 0, 0, 3, 6, 4, 5, 2],
+//   [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 4, 5, 2],
+//   [8, 1, 100, 0, 7, 2, 7, 3, 3, 0, 6, 4, 5, 2],
+//   [1, 9, 8, 5, 0, 7, 7, 3, 0, 3, 0, 4, 5, 2],
+//   [7, 3, 0, 3, 6, 4, 7, 3, 0, 3, 6, 0, 5, 2],
+//   [7, 3, 0, 3, 6, 0, 7, 3, 0, 3, 6, 4, 0, 2],
+//   [0, 2, 3, 4, 5, 7, 0, 3, 0, 3, 6, 4, 5, 0],
+// ];
+// fileweights is an object so we have to get the length somehow :)
 
 // Create a new instance of ws server
 const wsServer = new WebSocketServer({ server: appServer });
@@ -71,8 +73,7 @@ wsServer.on('connection', (webSocket) => {
   webSocket.send(JSON.stringify(id));
   webSocket.send(JSON.stringify(totalSubtasks));
 
-  const weightsObj = new Obj('weights', weights);
-  webSocket.send(JSON.stringify(weightsObj));
+  webSocket.send(JSON.stringify(fileWeightsObj));
 
   let problem = iterator.next();
 
